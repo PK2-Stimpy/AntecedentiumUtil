317db4a3c360b02559ee7a1a9309f0923281dbf1
PK2_Stimpy <31771533+PK2-Stimpy@users.noreply.github.com>
2021-02-07 12:43:58 +0100

    update LightLagListener.java

--- src/com/antecedentium/events/LightLagListener.java
+++ src/com/antecedentium/events/LightLagListener.java
@@ -15,7 +15,6 @@ import java.util.Map;
 public class LightLagListener implements Listener {
     HashMap<Player, Location> lastPlaced = new HashMap<>();
     HashMap<Player, Integer> amount = new HashMap<>();
-    HashMap<Player, Boolean> nigMap = new HashMap<>();
 
     AnteCedentium plugin;
     int maxSpam;
@@ -27,16 +26,6 @@ public class LightLagListener implements Listener {
         this.maxSpam = plugin.getConfig().getInt("modules.light-lag.config.max-spam");
         this.enabled = plugin.getConfig().getBoolean("modules.light-lag.enabled");
         this.kick = plugin.getConfig().getString("modules.light-lag.messages.kick");
-
-        BukkitTask bukkitTask = new BukkitRunnable() {
-            @Override
-            public void run() {
-                for (Map.Entry<Player, Integer> violationEntry : amount.entrySet()) {
-                    if (violationEntry.getValue() > 0)
-                        violationEntry.setValue(violationEntry.getValue() - 1);
-                }
-            }
-        }.runTaskTimer(plugin, 1L, 19L);
     }
 
     @EventHandler
@@ -45,24 +34,8 @@ public class LightLagListener implements Listener {
             return;
 
         Player player = event.getPlayer();
-        nigMap.putIfAbsent(player, false);
-        nigMap.put(player, !nigMap.get(player));
-        if (nigMap.get(player)) {
-            lastPlaced.put(player, event.getBlock().getLocation());
-        } else {
-            if (lastPlaced.get(player).equals(event.getBlock().getLocation())) {
-                if (amount.containsKey(player)) {
-                    amount.put(player, amount.get(player) + 1);
-                } else {
-                    amount.put(player, 0);
-                }
-                if (amount.get(player) >= maxSpam / 2) {
-                    player.kickPlayer(kick);
-                    lastPlaced.remove(player);
-                    amount.remove(player);
-                    nigMap.remove(player);
-                }
-            }
-        }
+        lastPlaced.put(player, event.getBlock().getLocation());
+        amount.put(player, (amount.contains(player)?amount.get(player):0)+1);
+        
     }
 }
